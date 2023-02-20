aa6fadced11ac8e93123c34050d8ce471c915ece
IJAZ KHAN. S <ijaskhan4343@gmail.com>
2023-02-20 09:35:17 +0530

    nb

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServiceComponent } from './service/service.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+const routes: Routes = [
+  {
+    path: '' , redirectTo: 'home', pathMatch:'full'
+  },
+  {
+    path:'home', component:HomeComponent
+  },
+  {
+    path:'about', component:AboutComponent
+  },
+  {
+    path:'contact', component:ContactComponent
+  },
+  {
+    path:'service', component:ServiceComponent
+  },
+  {
+    path:'login', component:LoginComponent
+  },
+  {
+    path:'signup', component:SignupComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
